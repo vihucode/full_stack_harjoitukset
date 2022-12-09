738b01c02855172225c33bff698c1266d8aa71d6
Vili Huotari <vili.huotari@tuni.fi>
2022-12-09 13:27:30 +0200

    Added step 11 to puhelinluettelo

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,20 +1,27 @@
-import { useState } from 'react'
-import Persons from './components/Persons'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
+import Persons from './components/Persons'
 import Filter from './components/Filter'
 import AddForm from './components/AddForm'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   const addName = (event) => {
     if (persons.filter(item => item.name.includes(newName)).length === 0) {
